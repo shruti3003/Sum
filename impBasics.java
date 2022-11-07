cecc385edf54379b92e8748c2c440ed3e418eabe
Shruti Vijay Tibile <84867329+shruti3003@users.noreply.github.com>
2022-11-07 23:02:11 +0530

    Add files via upload

---
+++ impBasics.java
@@ -0,0 +1,26 @@
+public class impBasics {
+    public static void main(String[]args){
+System.out.println("Hello world\n hello\n hi\n heyy by shruti");
+System.out.println("hi how are you");
+System.out.println("****");
+System.out.println("***");
+System.out.println("**");
+System.out.println("*");
+        //boilerplate code
+
+       byte b= 8;
+       System.out.println(b);
+       char ch= 'a';
+       System.out.println(ch);
+       boolean var= true;
+       System.out.println(var);
+       float price= 8.8f;
+       System.out.println(price);
+       double d= 8.8790;
+       System.out.println(d);
+       long c= 994672309l;
+       System.out.println(c);
+       
+    }
+    
+}
